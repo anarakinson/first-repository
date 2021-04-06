77ab42370962488a53b4c48927c208b0f783270b
anarakinson <anarakinson@gmail.com>
2021-04-06 20:51:08 +0300

    another commit

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
-i = 0
-while True:
-  print(i)
-  i += 1
-  if i == 10:
-    break
-
+j = input()
+if j.isdigit():
+  i = 0
+  j = int(j)
+  while True:
+    print(i)
+    i += 1
+    if i == j or i == 10:
+      break
+else:
+  print('input must be digit')
